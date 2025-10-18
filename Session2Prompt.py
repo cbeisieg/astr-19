8bad50c7c4e6b31840e930d9baf0ab62f0840faf
cbeisieg <cbeisieg@ucsc.edu>
2025-10-17 23:55:49 -0700

    Add files via upload

---
+++ Session2Prompt.py
@@ -0,0 +1,14 @@
+float1 = 6.2
+float2 = 32.5
+print(float1+float2)
+print("Data type: ", type(float1+float2))
+
+int1 = 128
+int2 = 61
+print(int1+int2)
+print("Data type: ", type(int1+int2))
+
+float3 = 23.1
+int3 = 5
+print(float3*int3)
+print("Data type: ", type(float3*int3))
