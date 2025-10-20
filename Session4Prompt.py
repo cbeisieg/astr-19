4ef9197e05df88c38a7fcf28d237b95695d68381
cbeisieg <cbeisieg@ucsc.edu>
2025-10-19 23:20:05 -0700

    Update Session4Prompt.py

--- Session4Prompt.py
+++ Session4Prompt.py
@@ -1,17 +1,23 @@
 class FavoriteAnimal:
-    def __init__(animal, arms, legs, eyes, tail, furry):
-        animal.arms = arms
-        animal.legs = legs
-        animal.eyes = eyes
-        animal.tail = tail
-        animal.furry = furry
+    def __init__(self, arms, legs, eyes, tail, furry):
+        self.arms = arms
+        self.legs = legs
+        self.eyes = eyes
+        self.tail = tail
+        self.furry = furry
 
-    def display_info(animal):
-        print("Arm length:", animal.arms, "inches")
-        print("Leg length:", animal.legs, "inches")
-        print("Number of eyes:", animal.eyes)
-        print("Does it have a tail:", animal.tail)
-        print("Is it furry:", animal.furry)
+    def display_info(self):
+        print("Arm length:", self.arms, "inches")
+        print("Leg length:", self.legs, "inches")
+        print("Number of eyes:", self.eyes)
+        print("Does it have a tail:", self.tail)
+        print("Is it furry:", self.furry)
 
-x = FavoriteAnimal(59.8, 59.8, 2, True, True)
-x.display_info()
+
+def main():
+    x = FavoriteAnimal(59.8, 59.8, 2, True, True)
+    x.display_info()
+
+
+if __name__ == "__main__":
+    main()
